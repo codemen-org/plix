b1054c8fac9c48091d3f64a09f4bf10bba003da2
tarafat <tanvir@appaloy.com>
2022-12-12 19:17:27 +0600

    pushed for test flight

--- android/app/src/main/kotlin/com/example/plix/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/plix/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.plix
+package com.plix.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD65q30OtTK6nTqbl3EfCIKDJhumzT3tXc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
